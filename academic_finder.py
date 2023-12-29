1693459d77092d5f48ea2ac173e7381ed4ff8de4
soyjherom <jherom@tecnica.co.cr>
2023-12-29 09:02:41 -0600

    Add multiple university support

--- academic_finder.py
+++ academic_finder.py
@@ -1,111 +1,123 @@
 import requests
 from bs4 import BeautifulSoup
 
-journals = [
+database = [
   {
-    "name": "Actualidades Investigativas En Educacion",
-    "field":"Educacion",
-    "url": "https://revistas.ucr.ac.cr/index.php/aie/search/"
+    "u_name": "UCR",
+    "u_url": "https://revistas.ucr.ac.cr/index.php/",
+    "journals":[
+      {
+        "name": "Actualidades Investigativas En Educacion",
+        "field":"Educacion",
+        "url": "aie/search/"
+      },
+      {
+        "name": "Cuadernos de Investigación y Formación en Educación Matemática",
+        "field":"Educacion",
+        "url": "cifem/search/" 
+      },
+      {
+        "name": "Revista Estudios",
+        "field":"Educacion",
+        "url": "estudios/search/"
+      },
+      {
+        "name": "Gestión de la Educación",
+        "field":"Educacion",
+        "url":"gestedu/search/"
+      },
+      {
+        "name": "Odovtos",
+        "field":"Educacion",
+        "url":"Odontos/search/"
+      },
+      {
+        "name": "Pensar en Movimiento",
+        "field":"Educacion",
+        "url":"pem/search/"
+      },
+      {
+        "name":"e-Ciencias de la Información",
+        "field":"Ingenieria",
+        "url":"eciencias/search/"
+      },
+      {
+        "name":"Ingeniería",
+        "field":"Ingenieria",
+        "url":"ingenieria/search/"
+      },
+      {
+        "name":"Revista de ciencia y tecnología",
+        "field":"Ingenieria",
+        "url":"cienciaytecnologia/search/"
+      },
+      {
+        "name":"Infraestructura Vial",
+        "field":"Ingenieria",
+        "url":"vial/search/"
+      },
+      {
+        "name":"InterSedes",
+        "field":"Interdisciplinar",
+        "url":"intersedes/search/"
+      },
+      {
+        "name":"Káñina",
+        "field":"Artes y letras",
+        "url":"kanina/search/"
+      },
+      {
+        "name":"Biología tropical",
+        "field":"Biologia",
+        "url":"rbt/search/"
+      },
+      {
+        "name":"Revista de ciencias economicas",
+        "field":"Economia",
+        "url":"economicas/search/"
+      },
+      {
+        "name":"Revista de ciencias juridicas",
+        "field":"Derecho",
+        "url":"juridicas/search/"
+      },
+      {
+        "name":"Revista de ciencias sociales",
+        "field":"Ciencias sociales",
+        "url":"sociales/search/"
+      },
+      {
+        "name":"Filología y Linguística de la UCR",
+        "field":"Ciencias sociales",
+        "url":"filyling/search/"
+      },
+      {
+        "name":"Filosofía de la UCR",
+        "field":"Ciencias sociales",
+        "url":"filosofia/search/"
+      },
+      {
+        "name":"",
+        "field":"",
+        "url":""
+      },
+      {
+        "name":"",
+        "field":"",
+        "url":""
+      },
+    ] 
   },
   {
-    "name": "Cuadernos de Investigación y Formación en Educación Matemática",
-    "field":"Educacion",
-    "url": "https://revistas.ucr.ac.cr/index.php/cifem/search/" 
-  },
-  {
-    "name": "Revista Estudios",
-    "field":"Educacion",
-    "url": "https://revistas.ucr.ac.cr/index.php/estudios/search/"
-  },
-  {
-    "name": "Gestión de la Educación",
-    "field":"Educacion",
-    "url":"https://revistas.ucr.ac.cr/index.php/gestedu/search/"
-  },
-  {
-    "name": "Odovtos",
-    "field":"Educacion",
-    "url":"https://revistas.ucr.ac.cr/index.php/Odontos/search/"
-  },
-  {
-    "name": "Pensar en Movimiento",
-    "field":"Educacion",
-    "url":"https://revistas.ucr.ac.cr/index.php/pem/search/"
-  },
-  {
-    "name":"e-Ciencias de la Información",
-    "field":"Ingenieria",
-    "url":"https://revistas.ucr.ac.cr/index.php/eciencias/search/"
-  },
-  {
-    "name":"Ingeniería",
-    "field":"Ingenieria",
-    "url":"https://revistas.ucr.ac.cr/index.php/ingenieria/search/"
-  },
-  {
-    "name":"Revista de ciencia y tecnología",
-    "field":"Ingenieria",
-    "url":"https://revistas.ucr.ac.cr/index.php/cienciaytecnologia/search/"
-  },
-  {
-    "name":"Infraestructura Vial",
-    "field":"Ingenieria",
-    "url":"https://revistas.ucr.ac.cr/index.php/vial/search/"
-  },
-  {
-    "name":"InterSedes",
-    "field":"Interdisciplinar",
-    "url":"https://revistas.ucr.ac.cr/index.php/intersedes/search/"
-  },
-  {
-    "name":"Káñina",
-    "field":"Artes y letras",
-    "url":"https://revistas.ucr.ac.cr/index.php/kanina/search/"
-  },
-  {
-    "name":"Biología tropical",
-    "field":"Biologia",
-    "url":"https://revistas.ucr.ac.cr/index.php/rbt/search/"
-  },
-  {
-    "name":"Revista de ciencias economicas",
-    "field":"Economia",
-    "url":"https://revistas.ucr.ac.cr/index.php/economicas/search/"
-  },
-  {
-    "name":"Revista de ciencias juridicas",
-    "field":"Derecho",
-    "url":"https://revistas.ucr.ac.cr/index.php/juridicas/search/"
-  },
-  {
-    "name":"Revista de ciencias sociales",
-    "field":"Ciencias sociales",
-    "url":"https://revistas.ucr.ac.cr/index.php/sociales/search/"
-  },
-  {
-    "name":"Filología y Linguística de la UCR",
-    "field":"Ciencias sociales",
-    "url":"https://revistas.ucr.ac.cr/index.php/filyling/search/"
-  },
-  {
-    "name":"Filosofía de la UCR",
-    "field":"Ciencias sociales",
-    "url":"https://revistas.ucr.ac.cr/index.php/filosofia/search/"
-  },
-  {
-    "name":"",
-    "field":"",
-    "url":""
-  },
-  {
-    "name":"",
-    "field":"",
-    "url":""
-  },
-  {
-    "name":"",
-    "field":"",
-    "url":""
+    "u_name": "",
+    "u_url": "",
+    "journals":[
+      {
+        "name":"",
+        "field":"",
+        "url":"",
+      }
+    ]
   },
 ]
 
@@ -130,9 +142,8 @@ class MyPrinter:
     print(f'{Colors.RED}Request error: {message}{Colors.RED}')
   def print_link(message):
     print(f'{Colors.BLUE}AT: {message}{Colors.BLUE}')
-  
 
-def set_search_criteria():
+def get_search_criteria():
   search_criteria = input('Please enter a search criteria without using punctuation symbols: ')
   search_criteria = search_criteria.replace(' ','+') if search_criteria else ''
   query="query="+search_criteria if search_criteria else ""
@@ -146,27 +157,32 @@ def set_search_criteria():
     search_criteria += "&dateToYear=" + date_to_year
   return search_criteria
 
-search_criteria = set_search_criteria()
+search_criteria = get_search_criteria()
 
-for journal in journals:
-  url = journal.get("url")
-  if url:
-    url += search_criteria
-    name = journal.get("name")
-    MyPrinter.print_field(f'Field: {journal.get("field")}')
-    MyPrinter.print_info(f'Searching into {name}')
-    MyPrinter.print_info(f'Using {url} Please wait...')
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-      soup = BeautifulSoup(response.text, 'lxml')
-      divs = soup.find_all('div', class_='article-summary-title')
-      if divs:
-        MyPrinter.print_warning(f'{len(divs)} Results')
-        for div in divs:
-          MyPrinter.print_finding(div.get_text(strip=True))
-          MyPrinter.print_link(div.find('a')['href'])
-      else:
-        MyPrinter.print_warning(f'{len(divs)} Results')
-    else:
-      MyPrinter.print_error(response.status_code)
+for university in database:
+  u_url = university.get("u_url")
+  u_name = university.get("u_name")
+  if u_url:
+    if u_name:
+      MyPrinter.print_info(f'Searching at {u_name}')
+    for journal in university.get("journals"):
+      url = u_url + journal.get("url") + search_criteria
+      name = journal.get("name")
+      if name:
+        MyPrinter.print_field(f'Field: {journal.get("field")}')
+        MyPrinter.print_info(f'Searching into {name}')
+        MyPrinter.print_info(f'Using {url} Please wait...')
+        response = requests.get(url)
+      
+        if response.status_code == 200:
+          soup = BeautifulSoup(response.text, 'lxml')
+          divs = soup.find_all('div', class_='article-summary-title')
+          if divs:
+            MyPrinter.print_warning(f'{len(divs)} Results')
+            for div in divs:
+              MyPrinter.print_finding(div.get_text(strip=True))
+              MyPrinter.print_link(div.find('a')['href'])
+          else:
+            MyPrinter.print_warning(f'{len(divs)} Results')
+        else:
+          MyPrinter.print_error(response.status_code)
